366e83242eb27727cb0236b4caec601745c7e41f
brauejm <brauejm@C001717184>
2020-10-18 17:25:10 -0600

    speed scaling pwm signal

--- DCMotorControl.cydsn/main.c
+++ DCMotorControl.cydsn/main.c
@@ -19,8 +19,13 @@ int main(void)
     PWM_1_Start();
 
     while(1)
-    {
-        
+    {   
+        int speeds[] = {0, 32, 64, 128, 192, 255};
+        int speed_size = sizeof(speeds) / sizeof(speeds[0]);
+        for(int i=0; i < speed_size; i++) {
+            PWM_1_WriteCompare(speeds[i]);
+            CyDelay(3000);
+        }
     }
 }
 
