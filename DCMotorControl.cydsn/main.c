a17c95d07baa818a2801036371d50a096dc17749
brauejm <brauejm@C001717184>
2020-10-25 14:55:28 -0600

    convert back emf to mV

--- DCMotorControl.cydsn/main.c
+++ DCMotorControl.cydsn/main.c
@@ -22,16 +22,22 @@ int main(void)
 
     while(1)
     {   
-        AMux_1_Select(1);
         int speeds[] = {0, 32, 64, 128, 192, 255};
         int speed_size = sizeof(speeds) / sizeof(speeds[0]);
         for(int i=0; i < speed_size; i++) {
             PWM_1_WriteCompare(speeds[i]);
             int delay_total = 3000;
-            /* Break into speed calc function later */
-            /* Calc speed between each delay request */
+            /* everything inside for loop will run 'during' total delay */
             for(int delay_count=0; i <= delay_total; i++) {
                 CyDelay(1); // Delay 1 millisecond
+                /* Break into speed calc function later */
+                /* Calc speed between each delay request */
+                AMux_1_Select(1);
+                ADC_DelSig_1_StartConvert();
+                ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT);
+                CyDelay(1000); // wait 100 microsec for conversion - IsEndConversion not working
+                uint16 Back_EMF_Counts = ADC_DelSig_1_GetResult16();
+                int16 Back_EMF_mV = ADC_DelSig_1_CountsTo_mVolts(Back_EMF_Counts);
             }
         }
     }
