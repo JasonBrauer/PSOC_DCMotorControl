5ed42a08f9c73ea20e32712deda50b30c2c732a1
brauejm <brauejm@C001717184>
2020-10-26 22:46:09 -0600

    speed change working, no back emf

--- DCMotorControl.cydsn/main.c
+++ DCMotorControl.cydsn/main.c
@@ -29,18 +29,18 @@ int main(void)
             int delay_total = 3000;
             /* everything inside for loop will run 'during' total delay */
             int lcd_write_counter = 0;
-            for(int delay_count=0; i <= delay_total; i=delay_count++) {
+            for(int delay_count=0; delay_count <= delay_total; delay_count++) {
                 CyDelay(1); // Delay 1 millisecond
                 /* Break into speed calc function later */
                 /* Calc speed between each delay request */
                 AMux_1_Select(1);
                 ADC_DelSig_1_StartConvert();
                 ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT);
-                CyDelay(1000); // wait 100 microsec for conversion - IsEndConversion not working
+                CyDelayUs(1000); // wait 100 microsec for conversion - IsEndConversion not working
                 uint16 back_emf_counts = ADC_DelSig_1_GetResult16();
                 int16 back_emf_mv = ADC_DelSig_1_CountsTo_mVolts(back_emf_counts);
-                /* only write to LCD every 1000 loops (1000 ms) */
-                if(lcd_write_counter++ > 1000) {
+                /* only write to LCD every 100 loops (100 ms) */
+                if(lcd_write_counter++ > 100) {
                     lcd_write_counter = 0; // reset write counter
                     LCD_Position(0,0);
                     //              -0123456789012345- 16 char guide
