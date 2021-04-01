2e91134e9412495791c1744e74334af000b3bb78
Matt Brauer <jbrauer90@gmail.com>
2021-03-31 23:36:08 -0600

    stub encoder functions

---
+++ DCMotorControl.cydsn/encoder.c
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+struct output {
+    int count;
+    char direction[5];
+};
+
+struct output readEncoder(int channela, int channelb)
+{
+};
+
+static char determineDirection(int channela, int channelb) 
+{
+};
+
+static int calcCount(int channelb)
+{
+};
+/* [] END OF FILE */
---
+++ DCMotorControl.cydsn/encoder.h
@@ -0,0 +1,18 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+struct output {
+    int count;
+    char direction[5];
+};
+
+struct output readEncoder(int channela, int channelb);
+/* [] END OF FILE */
