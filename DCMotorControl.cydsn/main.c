fce8fee49772e05283eba9722c9fc3c8e78f4b1b
Matt Brauer <jbrauer90@gmail.com>
2021-12-07 15:47:25 -0700

    remove troubleshooting code and use rotary encoder speed

--- DCMotorControl.cydsn/main.c
+++ DCMotorControl.cydsn/main.c
@@ -12,13 +12,8 @@
 #include "project.h"
 #include "rotaryencoder.h"
 
-static void outputIncrement();
-
 /* Global variables */
 volatile int input_speed = 0;
-// for increment status
-volatile int increment_global = 0;
-volatile int isr_count = 0;
 
 /*******************************************************************************
 * Function Name: encoder_interrupt
@@ -34,10 +29,7 @@ CY_ISR(encoder_interrupt)
     // encoder_status reading as 0 or 2 instead of expected 1 or 3
     encoder_status = Status_Reg_1_Read();
     int increment = readEncoder((int)encoder_status);
-    /// for increment status
-    increment_global = increment;
     input_speed = adjustSpeed(input_speed, increment);
-    isr_count++;
     Pin_12_4_ClearInterrupt();
 }
 
@@ -61,78 +53,41 @@ int main(void)
     
     
     int16 v_supply_mv = 4500;
-    
+    int delay_count = 0;
+
     while(1)
     {   
-        int speeds[] = {0, 32, 64, 128, 192, 255};
-        int speed_size = sizeof(speeds) / sizeof(speeds[0]);
-        for(int i=0; i < speed_size; i++) {
-            PWM_1_WriteCompare(speeds[i]);
-            int delay_total = 3000;
-            /* everything inside for loop will run 'during' total delay */
-            for(int delay_count=0; delay_count <= delay_total; delay_count++) {
-                /* write increment status to pins */
-                outputIncrement();
-                
-                CyDelay(1); // Delay 1 millisecond
-                /* Break into speed calc function later */
-                /* Calc speed between each delay request */
-                Control_Reg_1_Write(1);
-                CyDelayUs(200);
-                AMux_1_Select(0);
-                ADC_DelSig_1_StartConvert();
-                ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT);
-                CyDelayUs(5); // add delay for conversion to complete
-                Control_Reg_1_Write(0);
-                int32 back_emf_counts = (int32)ADC_DelSig_1_GetResult16();
-                int16 back_emf_mv = v_supply_mv - ADC_DelSig_1_CountsTo_mVolts(back_emf_counts);
-                /* only write to LCD every 100 loops (100 ms) */
-                if(delay_count % 500 == 0) {
-                    LCD_Position(0,0);
-                    //              -0123456789012345- 16 char guide
-                    LCD_PrintString("b_emf_mv:        ");
-                    LCD_Position(0,10);
-                    LCD_PrintNumber((uint16)back_emf_mv);
-                    
-                    LCD_Position(1,0);
-                    //              -0123456789012345- 16 char guide
-                    
-                    LCD_PrintString("input_speed:        ");
-                    LCD_Position(1,13);
-                    LCD_PrintNumber((uint16)input_speed);
-                    /*
-                    LCD_PrintString("isr_count:        ");
-                    LCD_Position(1,11);
-                    LCD_PrintNumber((uint16)isr_count);
-                    */
+        delay_count++;
+        PWM_1_WriteCompare(input_speed);
+        
+        CyDelay(1); // Delay 1 millisecond
+        /* Break into speed calc function later */
+        /* Calc speed between each delay request */
+        Control_Reg_1_Write(1);
+        CyDelayUs(200);
+        AMux_1_Select(0);
+        ADC_DelSig_1_StartConvert();
+        ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT);
+        CyDelayUs(5); // add delay for conversion to complete
+        Control_Reg_1_Write(0);
+        int32 back_emf_counts = (int32)ADC_DelSig_1_GetResult16();
+        int16 back_emf_mv = v_supply_mv - ADC_DelSig_1_CountsTo_mVolts(back_emf_counts);
+        /* only write to LCD every 100 loops (100 ms) */
+        if(delay_count % 500 == 0) {
+            LCD_Position(0,0);
+            //              -0123456789012345- 16 char guide
+            LCD_PrintString("b_emf_mv:        ");
+            LCD_Position(0,10);
+            LCD_PrintNumber((uint16)back_emf_mv);
             
-                }
-                /* longer motor off delay for scope inspection 
-                if(delay_count % 500 == 0) {
-                    Control_Reg_1_Write(1);
-                    CyDelay(10);
-                    Control_Reg_1_Write(0);
-                }
-                */
-            }
+            LCD_Position(1,0);
+            //              -0123456789012345- 16 char guide
+            
+            LCD_PrintString("input_speed:        ");
+            LCD_Position(1,13);
+            LCD_PrintNumber((uint16)input_speed);
         }
     }
 }
 
-
-static void outputIncrement()
-{
-     /* write increment status to pins */
-    if(increment_global == 1) {
-        Pin_15_1_Write(1u);
-    }
-    else if(increment_global == -1) {
-        Pin_15_2_Write(1u);
-    }
-    else {
-        Pin_15_1_Write(0u);
-        Pin_15_2_Write(0u);
-    }
-    increment_global = 0;
-};
 /* [] END OF FILE */
